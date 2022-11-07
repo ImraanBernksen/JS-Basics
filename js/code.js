94c7ae30976c7253e444f59c660d506d47c5597a
ImraanBernksen <imraanbernksen@gmail.com>
2022-11-07 16:45:46 +0200

    awe

---
+++ js/code.js
@@ -0,0 +1,40 @@
+// function madeBy() {
+//     alert("Made By Imraan Bernksen!!")
+// }
+
+// let userName = 'hello world';
+// let userAge = 20;
+// let isMale = true;
+
+// document.write(userName);
+// document.write("<br>" +userAge);
+// document.write("<br>" +isMale);
+// document.write(`<br>Name: ${userName}<br>Age: ${userAge}<br>Male: ${isMale}`);
+// console.log(typeof userName);
+// let names = ['Meezaan','Imraan','Brogan'];
+// console.log(names);
+
+// let salary = 100;
+// document.write(salary);
+// salary = 50000;
+// document.write(`<br>new salary : R${salary}`);
+// const vat = .16;
+// vat = .20;
+// document.write(vat)
+
+// let person = {
+//     name: 'John',
+//     suranme: 'Bruce'
+//     age: 26
+// };
+// console.log(person);
+
+// function awe() {
+//     console.log('Hello World');
+// }
+
+// awe()
+
+let nameMe=()=>{
+    console.log('imo')
+}
